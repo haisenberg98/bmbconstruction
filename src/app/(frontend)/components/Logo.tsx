286ff7a13c47144c50d89342f91916d8efcb9cbb
Denny Shu <denny.shu98@gmail.com>
2025-09-15 17:21:31 +1200

    Add class to Link component for improved styling

--- src/app/(frontend)/components/Logo.tsx
+++ src/app/(frontend)/components/Logo.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 const Logo = () => {
     return (
-        <Link href='/'>
+        <Link href='/' className='relative z-50'>
             <div className='logo flex flex-col items-center justify-center'>
                 <div>
                     <Image className='size-18' src='/images/logo.png' alt='logo' width={100} height={100} />
