e2f567a59211d7b746725c16f5b384c829541de3
Denny Shu <denny.shu98@gmail.com>
2025-09-15 17:17:25 +1200

    Refactor formatting for consistency in ContactPage component

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -1,22 +1,22 @@
-import ContactForm from "@/app/(frontend)/components/ContactForm";
-import ContactInfo from "@/app/(frontend)/components/ContactInfo";
+import ContactForm from '@/app/(frontend)/components/ContactForm';
+import ContactInfo from '@/app/(frontend)/components/ContactInfo';
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 
 export default function ContactPage() {
-  return (
-    <main id="contact-page" className="mb-12 px-4 xl:container md:px-8 xl:mx-auto xl:px-0">
-      <h1 className="mb-12">Get In Touch with Us</h1>
+    return (
+        <main id='contact-page' className='mb-12 px-4 xl:container md:px-8 xl:mx-auto xl:px-0'>
+            <h1 className='mb-8'>Get In Touch with Us</h1>
 
-      <section className="md:container md:mx-auto md:max-w-2xl lg:px-0">
-        <div className="relative overflow-hidden rounded-lg border border-gray-200 bg-white shadow-sm">
-          <ContactForm />
-        </div>
-      </section>
+            <section className='md:container md:mx-auto md:max-w-2xl lg:px-0'>
+                <div className='relative overflow-hidden rounded-lg border border-gray-200 bg-white shadow-sm'>
+                    <ContactForm />
+                </div>
+            </section>
 
-      <section className="mt-12 space-y-6 md:container md:mx-auto md:max-w-2xl lg:max-w-5xl lg:px-0">
-        <ContactInfo />
-      </section>
-    </main>
-  );
+            <section className='mt-12 space-y-6 md:container md:mx-auto md:max-w-2xl lg:max-w-5xl lg:px-0'>
+                <ContactInfo />
+            </section>
+        </main>
+    );
 }
